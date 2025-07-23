d876029e7969c76370a436a3c53f2e2fcfc589f6
Milosz Wach <wach.milosz1@gmail.com>
2025-07-23 13:39:36 +0200

    done

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,17 @@ import classes from './page.module.css';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({params}) {
+  const meal = await getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default async function MealDetailsPage({ params }) {
   const meal = await getMeal(params.mealSlug);
 
